66a7a8dc2bf44ac8b2d98c4462bebf583e943ed3
Suyash458 <sne9x@outlook.com>
2015-09-19 18:47:57 +0530

    Better plotting

--- analyse.py
+++ analyse.py
@@ -5,13 +5,13 @@ from operator import itemgetter
 import numpy as np
 import requests
 import io
-from bs4 import BeautifulSoup
 
 font = {'family' : 'sans-serif', 'weight' : 'normal', 'size' : 12}
 matplotlib.rc('font', **font)
 fig = plt.figure(figsize = (15,15))
+ax = fig.add_subplot(111)
 tokenCounter = collections.Counter(keyword.kwlist)
-maxLinks = 500
+maxLinks = 1
 
 for word in keyword.kwlist:
 	tokenCounter[word] = tokenCounter[word] - 1
@@ -22,6 +22,8 @@ def countKeywords():
 	with open('links.txt') as links:
 		for link in links:
 			try:
+				if processedLinks > maxLinks:
+					break
 				htmlFile = requests.get(link.rstrip()).text
 				with io.StringIO(htmlFile) as fileReader:
 					tokens = (token for _, token, _, _, _ in tokenize.generate_tokens(fileReader.readline))
@@ -30,8 +32,6 @@ def countKeywords():
 							processedKeywords = processedKeywords + 1
 							tokenCounter[token] = tokenCounter[token] + 1
 				processedLinks += 1
-				if processedLinks > maxLinks:
-					break
 				progressString = "\rProcessed {0} links and {1} keywords".format(processedLinks,processedKeywords)
 				print(progressString,end = '')
 			except:
@@ -44,13 +44,18 @@ def plotBarGraph(tokenCounts):
 	numElements = np.arange(len(tokenCounts[0]))
 	thickness = 0.45
 	topOffset = max(tokenCounts[1]) + len(str(max(tokenCounts[1])))
-	plt.ylim([0,topOffset])
-	plt.xlim([-1,len(tokenCounts[0])])
-	plt.xticks(numElements,tokenCounts[0],rotation = 55, verticalalignment = 'top')
-	rects = plt.bar(numElements,tokenCounts[1], width = thickness, linewidth = 1.5, edgecolor = 'black', color = 'green', align = 'center')
+	ax.set_title('Keywords vs Occurnces')
+	ax.set_xlabel('Keywords')
+	ax.set_ylabel('Occurences')
+	ax.xaxis.set_label_coords(1.05, 0.015)
+	ax.set_xticks(numElements)
+	ax.set_xticklabels(tokenCounts[0],rotation = 55, verticalalignment = 'top')
+	ax.set_ylim([0,topOffset])
+	ax.set_xlim([-1,len(tokenCounts[0])])
+	rects = ax.bar(numElements,tokenCounts[1], width = thickness, linewidth = 1.5, edgecolor = 'black', color = 'green', align = 'center')
 	for rect,count in zip(rects,tokenCounts[1]):
 		height = rect.get_height()
-		plt.text(rect.get_x()+rect.get_width()/2., 1.01*height, '%d'% count,
+		ax.text(rect.get_x()+rect.get_width()/2., 1.01*height, '%d'% count,
 			ha='center', va='bottom')
 	plt.show()
 			
